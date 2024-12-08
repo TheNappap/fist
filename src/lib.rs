f6cfb30be70d8fa5899f24c8221bafff6f2f0a6f
mathijsd <napoleonpapiertjes@gmail.com>
2024-12-08 22:18:49 +0100

    Prevent visibility warning, fist internals shouldn't be public

--- src/lib.rs
+++ src/lib.rs
@@ -102,7 +102,9 @@ impl<T: ?Sized, const SIZE: usize> DerefMut for Fist<T, SIZE> {
 }
 
 /// Dynamic Fist
-pub enum DynFist<T: ?Sized, const SIZE: usize> {
+pub struct DynFist<T: ?Sized, const SIZE: usize>(_DynFist<T, SIZE>);
+
+enum _DynFist<T: ?Sized, const SIZE: usize> {
     /// Stack Type
     Fist(_Fist<T, SIZE>),
     /// Heap Type
@@ -122,9 +124,9 @@ impl<T: ?Sized, const SIZE: usize> DynFist<T, SIZE> {
     /// ```
     pub fn new<V: Unsize<T>>(v: V) -> DynFist<T, SIZE> {
         if mem::size_of::<V>() <= SIZE {
-            DynFist::Fist(_Fist::new(v))
+            DynFist(_DynFist::Fist(_Fist::new(v)))
         } else {
-            DynFist::Box(Box::<V>::new(v))
+            DynFist(_DynFist::Box(Box::<V>::new(v)))
         }
     }
 
@@ -140,7 +142,7 @@ impl<T: ?Sized, const SIZE: usize> DynFist<T, SIZE> {
     /// assert!(dynfist_stack.on_stack());
     /// ```
     pub fn on_stack(&self) -> bool {
-        matches!(self, DynFist::Fist(_))
+        matches!(self.0, _DynFist::Fist(_))
     }
 
     /// Returns `true` if the owned value is on heap.
@@ -155,7 +157,7 @@ impl<T: ?Sized, const SIZE: usize> DynFist<T, SIZE> {
     /// assert!(dynfist_heap.on_heap());
     /// ```
     pub fn on_heap(&self) -> bool {
-        matches!(self, DynFist::Box(_))
+        matches!(self.0, _DynFist::Box(_))
     }
 }
 
@@ -163,18 +165,18 @@ impl<T: ?Sized, const SIZE: usize> Deref for DynFist<T, SIZE> {
     type Target = T;
 
     fn deref(&self) -> &T {
-        match self {
-            DynFist::Fist(ref f) => f.deref(),
-            DynFist::Box(ref b) => b.deref(),
+        match self.0 {
+            _DynFist::Fist(ref f) => f.deref(),
+            _DynFist::Box(ref b) => b.deref(),
         }
     }
 }
 
 impl<T: ?Sized, const SIZE: usize> DerefMut for DynFist<T, SIZE> {
     fn deref_mut(&mut self) -> &mut T {
-        match self {
-            DynFist::Fist(ref mut f) => f.deref_mut(),
-            DynFist::Box(ref mut b) => b.deref_mut(),
+        match self.0 {
+            _DynFist::Fist(ref mut f) => f.deref_mut(),
+            _DynFist::Box(ref mut b) => b.deref_mut(),
         }
     }
 }
